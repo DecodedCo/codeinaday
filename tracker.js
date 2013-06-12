cd96fde94ae3a1c48c55723a2099e07b74c36f0a
Ali Blackwell <aliblackwell@gmail.com>
2013-06-12 18:50:52 +0100

    Removed watchPosition boilerplate code

--- tracker.js
+++ tracker.js
@@ -1,17 +1,9 @@
 // Wait for the HTML to load
 $(document).ready(getStarted);
 
+// Do this when it's loaded
 function getStarted(){	
-	// watch the user's location
-	watchUser = navigator.geolocation.watchPosition(calculateDistance);
-
 
 
 } // END getStarted
 
-// Our function to pass to watchPosition when it finds user
-function calculateDistance(position){
-
-
-
-} // END calculateDistance
