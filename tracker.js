c2d89f1dd35b07fb5c5a4ab5b2bd85ffcb1e536e
Amadeus <github@amadeus.me.uk>
2013-07-24 18:13:53 -0400

    Update tracker.js

--- tracker.js
+++ tracker.js
@@ -7,15 +7,4 @@ function getStarted() {
 
 
 
-  // track the user's location
-  watchUser = navigator.geolocation.watchPosition(calculateDistance);
-
 } // END getStarted
-
-// Function to run when the user's position is found
-function calculateDistance(position) {
-  
-  
-  
-  
-} // END calculateDistance
