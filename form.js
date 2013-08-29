b3e3c1c7e78d3d7951a503e594fb44a918e54420
Tim Ruffles <timruffles@googlemail.com>
2013-08-29 18:55:58 +0100

    don't bother waiting for document ready as it's unneeded - a) should be immediately before closing body for performance, b) just adds complexity. Also move ajaxCache out of the way, distracting

--- form.js
+++ form.js
@@ -1,32 +1,28 @@
+// Main function: process form input via AJAX
+// When someone clicks on the button...
+$("form").submit(function() {
+
+    var username = $('input:first').val();
+    $.ajax({
+         url: 'http://api.decoded.co/checkin',
+         data: {username: username},
+         method: 'POST',
+         dataType: 'jsonp',
+         jsonp: 'callback',
+         jsonpCallback: 'jsonpCallback',
+         success: function(data){
+             console.log(data);
+             // Display messages
+             $('#message').html('Hello there @' + data.username + '! Number of checkins: ' + data.checkIns);
+             $('form').hide();
+             if (typeof watchUser != 'undefined') navigator.geolocation.clearWatch(watchUser);
+        }
+    });
+    // Allow form to submit without reloading the page
+    return false;
+});
+
 // Stop AJAX caching 
 jQuery.ajaxSetup({ cache:false });
 
-// Wait for the page to load - $ is a shortcut for jQuery
-$(document).ready(interceptSubmit);
 
-// Main function: process form input via AJAX
-function interceptSubmit() {
-    // 1 When someone clicks on the button...
-    $("form").submit(function() {
-
-        var username = $('input:first').val();
-           $.ajax({
-                url: 'http://api.decoded.co/checkin',
-                data: {username: username},
-                method: 'POST',
-                dataType: 'jsonp',
-                jsonp: 'callback',
-                jsonpCallback: 'jsonpCallback',
-                success: function(data){
-                    console.log(data);
-                    // Display messages
-                    $('p#message').html('Hello there @' + data.username + '! Number of checkins: ' + data.checkIns);   
-                    $('form').hide();
-                    if (typeof watchUser != 'undefined') navigator.geolocation.clearWatch(watchUser);
-                }
-            });
-        return false;
-    })
-    // Allow form to submit without reloading the page 
-}
-  
