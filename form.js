01c6ce5e29500c3f1ffe15eddfa18666c097bc72
Amadeus Stevenson <amadeus@decoded.us>
2013-10-31 13:20:36 +0000

    Simplify and comment

--- form.js
+++ form.js
@@ -1,32 +1,30 @@
-// Stop AJAX caching 
-jQuery.ajaxSetup({ cache:false });
+// 1. Wait for the page to load, then run interceptSubmit
+jQuery(document).ready(interceptSubmit);
 
-// Wait for the page to load - $ is a shortcut for jQuery
-$(document).ready(interceptSubmit);
-
-// Main function: process form input via AJAX
+// Function to intercept the submit process
 function interceptSubmit() {
-    // 1 When someone clicks on the button...
+    // 2. When someone clicks on the button...
     $("form").submit(function() {
-
-        var username = $('input:first').val();
-           $.ajax({
-                url: 'http://api.decoded.co/checkin',
-                data: {username: username},
-                method: 'POST',
-                dataType: 'jsonp',
-                jsonp: 'callback',
-                jsonpCallback: 'jsonpCallback',
-                success: function(data){
-                    console.log(data);
-                    // Display messages
-                    $('p#message').html('Hello there @' + data.username + '! Number of checkins: ' + data.checkIns);   
-                    $('form').hide();
-                    if (typeof watchUser != 'undefined') navigator.geolocation.clearWatch(watchUser);
-                }
-            });
+    // $ is a shortcut for jQuery
+        // 3. Perform an AJAX request
+        $.ajax({
+            // 4. Use the URL from the form ...
+            url: $('form').attr('action'),
+            // 5. Send the username from the input
+            data: { username: $('input').val() },
+            // 6. If it has worked, take the result
+            success: function(result){
+                // 7. Change the paragraph with an id 'message'
+                $('p#message').html('Hello there ' + result.username + '! Number of checkins: ' + result.checkIns);   
+                // 8. Hide the form now the user has checked in
+                $('form').hide();
+                // 9. Once they have checked in, stop watching their position
+                if (typeof watchUser != 'undefined') 
+                    navigator.geolocation.clearWatch(watchUser);
+            } // END success
+        }); // END ajax
+        
+        // 10. Allow form to submit without reloading the page
         return false;
-    })
-    // Allow form to submit without reloading the page 
-}
-  
+    }) // END submit
+} // END interceptSubmit
