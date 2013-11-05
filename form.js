b25d0b2db27471a286362104ff580a5f71222ca9
johnridpath <jsridpath@gmail.com>
2013-11-05 17:02:25 +0000

    Tweaked comments in form.js

--- form.js
+++ form.js
@@ -1,20 +1,18 @@
 // 1. Wait for the page to load, then run interceptSubmit
 jQuery(document).ready(interceptSubmit);
 
-// Function to intercept the submit process
 function interceptSubmit() {
-    // 2. When someone clicks on the button...
+    // 2. When someone submits the form ($ is a shortcut for jQuery):
     $("form").submit(function() {
-    // $ is a shortcut for jQuery
-        // 3. Perform an AJAX request
+        // 3. Perform an AJAX request:
         $.ajax({
-            // 4. Use the URL from the form ...
+            // 4. Where to send data: use the URL from the form's action attribute
             url: $('form').attr('action'),
-            // 5. Send the username from the input
+            // 5. What data to send: send the username specified in form input 
             data: { username: $('input').val() },
-            // 6. If it has worked, take the result
+            // 6. What to do if data submits successfully:
             success: function(result){
-                // 7. Change the paragraph with an id 'message'
+                // 7. Change the paragraph with an id 'message' to display a welcome message
                 $('p#message').html('Hello there ' + result.username + '! Number of checkins: ' + result.checkIns);   
                 // 8. Hide the form now the user has checked in
                 $('form').hide();
