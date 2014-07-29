8143c632a98669ffa948b26c2fd6dda688a40544
Harriet Pellereau <harriet@decoded.co>
2014-07-29 11:09:19 +0100

    New Tracker Boilerplate

---
+++ tracker.js
@@ -0,0 +1,9 @@
+// watch the user's location
+watchUser = navigator.geolocation.watchPosition(successFunction, errorFunction);
+
+// successFunction is run when watchPosition method is successful 
+function successFunction(position){
+
+	// Contents of successFuncton
+
+} // END successFunction
