84cfb195c099b22dcef7f409e532fdd2073ff1a6
Harriet Pellereau <harrietp@users.noreply.github.com>
2014-07-31 18:33:15 +0100

    Removed watchUser global function

--- tracker.js
+++ tracker.js
@@ -1,5 +1,5 @@
-// Watch the user's location using geolocation
-watchUser = navigator.geolocation.watchPosition(successFunction, errorFunction);
+// Find the user's location using geolocation
+navigator.geolocation.watchPosition(successFunction, errorFunction);
 
 // successFunction is run when watchPosition method is successful 
 function successFunction(position){
@@ -7,7 +7,7 @@ function successFunction(position){
 	// Test if successFuncton worked in console
 	console.log("Tracking worked!")
 
-	// Extracting user locaiton information from position object 
+	// Extract user location information from position object 
 	var userLat = position.coords.latitude;
 	var userLon = position.coords.longitude;
 
