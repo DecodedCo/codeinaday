caea1c1e0b59b8df36d7230dab532f0bc79a7490
Harriet Pellereau <harrietp@users.noreply.github.com>
2014-08-06 17:50:20 +0100

    Changed ' to " for consistency

--- form.js
+++ form.js
@@ -1,19 +1,19 @@
 // 1. When someone submits the form with an id 'checkin':
-jQuery('form#checkin').submit(function() {
+jQuery("form#checkin").submit(function() {
     // 2. Perform an AJAX request ($ is a shortcut for jQuery):
     $.ajax({
         // 3. Where to send data: use the URL from the form's action attribute
-        url: $('form#checkin').attr('action'),
+        url: $("form#checkin").attr("action"),
         // 4. Send the username from the input
-        data: { username: $('input').val() },
+        data: { username: $("input").val() },
         // 5. What to do if data submits successfully:
         success: function(result){
             // 6. Change the paragraph with an id 'message' to display a welcome message
-            $('p#message').html('Hello there ' + result.username + '! Number of checkins: ' + result.checkIns);   
+            $("p#message").html("Hello there " + result.username + "! Number of checkins: " + result.checkIns);   
             // 7. Hide the form now the user has checked in
-            $('form#checkin').hide();
+            $("form#checkin").hide();
             // 8. Once they have checked in, stop watching their position
-            if (typeof watchUser != 'undefined') 
+            if (typeof watchUser != "undefined") 
                 navigator.geolocation.clearWatch(watchUser);
         } // END success
     }); // END ajax
