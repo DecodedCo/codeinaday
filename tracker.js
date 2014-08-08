109ecc255a3226c9c4d38cf18758474ac976a074
Harriet Pellereau <harrietp@users.noreply.github.com>
2014-08-08 15:08:37 +0100

    Comment Edit

--- tracker.js
+++ tracker.js
@@ -1,13 +1,13 @@
 // Find the users location using geolocation
 watchUser = navigator.geolocation.watchPosition(success, error);
 
-// successFunction is run when watchPosition is successful 
+// success is run when watchPosition is successful 
 function success(position){
 
-	// Test if successFuncton worked in console
+	// Test if tracking worked in console
 	console.log("Tracking worked!");
 
-	// Look at object created by watchPosition 
+	// Look at position object created by watchPosition 
 	console.log(position);
 
-} // END successFunction
+} // END success
