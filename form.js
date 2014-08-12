a91cd5a0af3cc62372102974d57238cbd4593cd8
Harriet Pellereau <harrietp@users.noreply.github.com>
2014-08-12 12:13:13 +0100

    Removed checkin id for beta

--- form.js
+++ form.js
@@ -1,9 +1,9 @@
-// 1. When someone submits the form with an id 'checkin':
-jQuery("form#checkin").submit(function() {
+// 1. When someone submits a form:
+jQuery("form").submit(function() {
     // 2. Perform an AJAX request ($ is a shortcut for jQuery):
     $.ajax({
         // 3. Where to send data: use the URL from the form's action attribute
-        url: $("form#checkin").attr("action"),
+        url: $("form").attr("action"),
         // 4. Send the username from the input
         data: { username: $("input").val() },
         // 5. What to do if data submits successfully:
@@ -11,7 +11,7 @@ jQuery("form#checkin").submit(function() {
             // 6. Change the paragraph with an id 'message' to display a welcome message
             $("p#message").html("Hello there " + result.username + "! Number of checkins: " + result.checkIns);   
             // 7. Hide the form now the user has checked in
-            $("form#checkin").hide();
+            $("form").hide();
             // 8. Once they have checked in, stop watching their position
             if (typeof watchUser != "undefined") 
                 navigator.geolocation.clearWatch(watchUser);
