51dffcb7066f952fc4eb88a0fe3c7ee317a95f6e
Harriet Pellereau <harrietp@users.noreply.github.com>
2014-08-13 16:53:30 +0100

    Edit to tracking comment & position

--- tracker.js
+++ tracker.js
@@ -5,10 +5,10 @@ watchUser = navigator.geolocation.watchPosition(success, error);
 function success(position){
 
 	// Test if tracking worked in browser console
-	console.log("Tracking was successful");
+	console.log("Tracking was successful!");
 	
 	// View position object in browser console
-	console.log("position");
+	console.log(position);
 
 	// Capture user location coordinates in variables
 	var userLat = position.coords.latitude;
