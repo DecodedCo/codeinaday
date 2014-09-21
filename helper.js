330fd53d7096f281260c85d3f69fc584fd5632c1
Harriet Pellereau <harriet@decoded.co>
2014-09-22 08:12:45 +1000

    New helper file

--- helper.js
+++ helper.js
@@ -1,3 +1,9 @@
+// Hide form to disallow checkin
+$("form").hide();
+
+// Change message
+$("p#message").html("Please allow tracking... :)");
+
 // Find the users location using geolocation
 watchUser = navigator.geolocation.watchPosition(success, error);
 
@@ -15,3 +21,30 @@ function success(position){
 	var userLon = position.coords.longitude;
 
 } // END success
+
+
+
+
+
+
+
+
+// Calculate distance function research
+// http://stackoverflow.com/questions/27928/how-do-i-calculate-distance-between-two-latitude-longitude-points
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2-lat1);  // deg2rad below
+  var dLon = deg2rad(lon2-lon1); 
+  var a = 
+    Math.sin(dLat/2) * Math.sin(dLat/2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+    Math.sin(dLon/2) * Math.sin(dLon/2)
+    ; 
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
+}
