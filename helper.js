67542815c1a494d27358b483f1365bff9c56935f
Amadeus <github@amadeus.me.uk>
2014-10-20 17:11:24 -0400

    Fix revert

--- helper.js
+++ helper.js
@@ -1,8 +1,8 @@
 // Find the users location using geolocation
-watchUser = navigator.geolocation.watchPosition(success);
+watchUser = navigator.geolocation.watchPosition(success,error);
 
 // success is run when watchPosition is successful
-function success(position,error) {
+function success(position) {
 
 	// Test if tracking worked in browser console
 	console.log("Tracking was successful!");
