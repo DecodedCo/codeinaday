dc7e625be071f72a24d89ec46efc68a1cb14c69b
Amadeus Stevenson <amadeus@decoded.us>
2014-10-20 08:47:49 -0400

    Hotfix: remove error callback

--- helper.js
+++ helper.js
@@ -1,12 +1,12 @@
 // Find the users location using geolocation
-watchUser = navigator.geolocation.watchPosition(success,error);
+watchUser = navigator.geolocation.watchPosition(success);
 
-// success is run when watchPosition is successful 
+// success is run when watchPosition is successful
 function success(position){
 
 	// Test if tracking worked in browser console
 	console.log("Tracking was successful!");
-	
+
 	// View position object in browser console
 	console.log(position);
 
