01f41601265daf25654f68d0a375c4165b49143c
Harriet Pellereau <harrietp@users.noreply.github.com>
2014-10-31 09:42:23 +0000

    Update helper

--- helper.js
+++ helper.js
@@ -1,7 +1,7 @@
 // Find the users location using geolocation
 watchUser = navigator.geolocation.watchPosition(success, error);
 
-// success is run when watchPosition is successful 
+// Success is run when watchPosition is successful 
 function success(position){
 
 	// Test if tracking worked in browser console
