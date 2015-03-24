7704a6b07a355760e1ff0feb56f0512b879dc93a
Olly <oliver@decoded.co>
2015-03-24 12:47:18 +0100

    chagne

--- form.js
+++ form.js
@@ -9,15 +9,15 @@ jQuery("form").submit(function() {
         // 5. What to do if data submits successfully:
         success: function(result){
             // 6. Change the paragraph with an id 'message' to display a welcome message
-            $("p#message").html("Hello there " + result.username + "! Number of checkins: " + result.checkIns);   
-            // 7. Hide the form now the user has checked in
+            $("p#message").html("Hello there " + result.username + "! You have registered: " + result.checkIns + " products");
+            // 7. Hide the form now the user has registered their product
             $("form").hide();
-            // 8. Once they have checked in, stop watching their position
-            if (typeof watchUser != "undefined") 
+            // 8. Once they have registered, stop watching their position
+            if (typeof watchUser != "undefined")
                 navigator.geolocation.clearWatch(watchUser);
         } // END success
     }); // END ajax
-    
+
     // 9. Allow form to submit without reloading the page
     event.preventDefault();
 }) // END submit
