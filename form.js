8ae6c0bcef41a25080525a07439e013e5d666243
Olly <oliver@decoded.co>
2015-04-21 10:30:17 +0200

    philips update

--- form.js
+++ form.js
@@ -1,20 +1,19 @@
 // 1. When someone submits a form:
 jQuery("form").submit(function() {
-    // 2. Perform an AJAX request ($ is a shortcut for jQuery):
+    // 2. Define the id as a random number
+    id = Math.floor(Math.random()*10000);
+    // 3. Perform an AJAX request ($ is a shortcut for jQuery):
     $.ajax({
-        // 3. Where to send data: use the URL from the form's action attribute
+        // 4. Where to send data: use the URL from the form's action attribute
         url: $("form").attr("action"),
-        // 4. Send the username from the input
-        data: { username: $("input[name=username]").val() },
-        // 5. What to do if data submits successfully:
+        // 5. Send the data from the input (with out id)
+        data: "id=" + id + "&" + $("form").serialize(),
+        // 6. What to do if data submits successfully:
         success: function(result){
-            // 6. Change the paragraph with an id 'message' to display a welcome message
-            $("p#message").html("Hello there " + result.username + "! You have registered: " + result.checkIns + " products");
-            // 7. Hide the form now the user has registered their product
+            // 7. Change the paragraph with an id 'message' to display a thank you message
+            $("p#message").html("Hello there " + result.id + "! Thanks for your information");
+            // 8. Hide the form now the user has given us information in
             $("form").hide();
-            // 8. Once they have registered, stop watching their position
-            if (typeof watchUser != "undefined")
-                navigator.geolocation.clearWatch(watchUser);
         } // END success
     }); // END ajax
 
