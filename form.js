c65b882fa9d8b3b15fb81244a3948b16e5ced79f
Olly <oliver@decoded.co>
2015-08-19 15:27:48 +0100

    updates for august:

--- form.js
+++ form.js
@@ -1,13 +1,10 @@
-// Pull in jQuery
-  var script = document.createElement('script');
-  script.src = '//code.jquery.com/jquery-1.11.0.min.js';
-  script.type = 'text/javascript';
-  document.getElementsByTagName('head')[0].appendChild(script);
+var message = "Thank you for registering an interest.";
 
+function startPage() {
 // 1. When someone submits a form:
 jQuery("form").submit(function() {
     // 2. Define the id as a random number
-    id = Math.floor(Math.random()*10000);
+    id = new Date().getTime();
     // 3. Perform an AJAX request ($ is a shortcut for jQuery):
     $.ajax({
         // 4. Where to send data: use the URL from the form's action attribute
@@ -16,14 +13,20 @@ jQuery("form").submit(function() {
         data: "id=" + id + "&" + $("form").serialize(),
         // 6. What to do if data submits successfully:
         success: function(result){
-            // 7. Hide the form now the user has given us information in
-            $("form").hide();
+            // 7. Hide the form now the user has given us information in (and change the message).
+            $("form").html(message);
+
         } // END success
     }); // END ajax
 
     // 9. Allow form to submit without reloading the page
     event.preventDefault();
 }) // END submit
+}
 
-
-
+// Pull in jQuery
+  var jq = document.createElement('script');
+  jq.src = '//code.jquery.com/jquery-1.11.0.min.js';
+  jq.type = 'text/javascript';
+  document.getElementsByTagName('head')[0].appendChild(jq);
+    jq.onload = startPage;
