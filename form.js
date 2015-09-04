335d34e55f28d6ec67262383d7ad7cefd1a81520
Olly <oliver@decoded.co>
2015-09-04 12:34:56 +0100

    removed the need for an ID

--- form.js
+++ form.js
@@ -3,23 +3,20 @@ var message = "Thank you for registering an interest.";
 function startPage() {
 // 1. When someone submits a form:
 jQuery("form").submit(function() {
-    // 2. Define the id as the current time
-    id = new Date().getTime();
-    // 3. Perform an AJAX request ($ is a shortcut for jQuery):
+    // 2. Perform an AJAX request ($ is a shortcut for jQuery):
     $.ajax({
-        // 4. Where to send data: use the URL from the form's action attribute
+        // 3. Where to send data: use the URL from the form's action attribute
         url: $("form").attr("action"),
-        // 5. Send the data from the input (with out id)
-        data: "id=" + id + "&" + $("form").serialize(),
-        // 6. What to do if data submits successfully:
+        // 4. Send the data from the input (with out id)
+        data: $("form").serialize(),
+        // 5. What to do if data submits successfully:
         success: function(result){
-            // 7. Hide the form now the user has given us information in (and change the message).
+            // 6. Hide the form now the user has given us information in (and change the message).
             $("form").html(message);
 
         } // END success
     }); // END ajax
-
-    // 9. Allow form to submit without reloading the page
+    // 7. Allow form to submit without reloading the page
     event.preventDefault();
 }) // END submit
 }
