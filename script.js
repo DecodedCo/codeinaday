309031083c99d5936f61cfd1252cadb713b6b8bf
Harriet Pellereau <harriet@decoded.co>
2017-01-26 15:01:38 +0000

    Updated script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-// script for tracking
+// Find the users location using geolocation
+watchUser = navigator.geolocation.watchPosition(success);
+
+// success is run when watchPosition is successful
+function success(position){
+
+  // Test if tracking worked in browser console
+  console.log("Tracking was successful");
+
+  // View position object in browser console
+  console.log(position);
+
+  // Capture user location coordinates in variables
+  var userLat = position.coords.latitude;
+  var userLon = position.coords.longitude;
+
+} // END success
+
 
