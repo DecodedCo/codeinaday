254c9937aeeedce82134be998d4ba6632c382627
johnridpath <jsridpath@gmail.com>
2017-12-11 10:03:10 +0000

    Update helper.js

--- helper.js
+++ helper.js
@@ -1,30 +1,13 @@
 // Find the users location using geolocation
-navigator.geolocation.watchPosition(success);
+watchUser = navigator.geolocation.watchPosition(success, error);
 
-// Write out code here!
+// Success is run when watchPosition is successful 
+function success(position){
 
+	// Test if tracking worked in browser console
+	console.log("Tracking was successful!");
+	
+	// View position object in browser console
+	console.log(position);
 
-
-
-
-
-
-
-// From https://stackoverflow.com/questions/27928/
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1); 
-  var a = 
-    Math.sin(dLat/2) * Math.sin(dLat/2) +
-    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
-    Math.sin(dLon/2) * Math.sin(dLon/2)
-    ; 
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-  var d = R * c; // Distance in km
-  return d;
-}
-
-function deg2rad(deg) {
-  return deg * (Math.PI/180)
-}
+} // END success
